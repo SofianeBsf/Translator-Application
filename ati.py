eb3fbca8481a50fbe36dc253f95a99c83849c9bd
sofiane <soufine2011@outlook.com>
2024-04-05 17:31:06 +0100

    commit

--- ati.py
+++ ati.py
@@ -8,3 +8,4 @@ root.geometry("700x400")
 root.title("Language Translator")
 root.config(bg = '#D3D3D3')
 
+Label(root, text = "Language Translator-PythonFlood", font = ('Arial, bold', 20), bg = '#8B8B7A')

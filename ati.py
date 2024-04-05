a85af61044164a5908f7077c1ea7d4e45af6e4ec
sofiane <soufine2011@outlook.com>
2024-04-05 18:43:03 +0100

    commit

--- ati.py
+++ ati.py
@@ -8,10 +8,15 @@ root.title('Translator')
 root.iconbitmap('#')
 root.geometry('880x300')
 
+def translate():
+    pass
+
 original_text = Text(root, height=10, width=40)
 original_text.grid(row=0, column=0, padx=10, pady=20)
 
 translate_button = Button(root, text="TRANSLATE!", font=("Arial", 24), command=translate)
+translate_button.grid(row=0, column=1, padx=10)
 
 translated_text = Text(root, height=10, width=40)
 translated_text.grid(row=0, column=2, padx=10, pady=20)
+

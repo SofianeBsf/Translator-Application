100b8d066a7db31943748e064118bd56d2b86a29
sofiane <soufine2011@outlook.com>
2024-04-05 18:38:30 +0100

    commit

--- ati.py
+++ ati.py
@@ -6,3 +6,7 @@ from tkinter import ttk, messagebox
 root = Tk()
 root.title('Translator')
 root.iconbitmap('#')
+root.geometry('880x300')
+
+original_text = Text(root, height=10, width=40)
+original_text.grid(row=0, column=0, padx=10, pady=20)
